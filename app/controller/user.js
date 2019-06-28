6c02ece96853bb1c974337a985828236e63b49c5
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-28 13:28:29 +0700

    add auth to get by id

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,23 +1,23 @@
-const router = require('express').Router();
-const userService = require('../service/user');
-const auth = require('../helper/auth');
-router.get('', (req, res) => {
+const router = require("express").Router();
+const userService = require("../service/user");
+const auth = require("../helper/auth");
+router.get("", (req, res) => {
   res.json(userService.findAll());
 });
 
-router.post('', (req, res) => {
+router.post("", (req, res) => {
   res.json(userService.create(req.body));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", auth.authenticate(), (req, res) => {
   res.json(userService.getById(req.params.id));
 });
 
-router.put('/:id', auth.authenticate(), (req, res) => {
+router.put("/:id", auth.authenticate(), (req, res) => {
   res.json(userService.update(req.params.id, req.body));
 });
 
-router.delete('/:id', auth.authenticate(), (req, res) => {
+router.delete("/:id", auth.authenticate(), (req, res) => {
   res.json(userService.delete(req.params.id));
 });
 
